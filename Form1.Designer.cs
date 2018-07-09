3a440ee6c8f4f41ced73cf368d095a824d0a25bb
liujj05 <liujj05@users.noreply.github.com>
2018-07-09 14:10:04 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace SerialportSample
+{
+    partial class SerialportSampleForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.DataPoint dataPoint1 = new System.Windows.Forms.DataVisualization.Charting.DataPoint(0D, 0D);
+            this.comboPortName = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBaudrate = new System.Windows.Forms.ComboBox();
+            this.buttonOpenClose = new System.Windows.Forms.Button();
+            this.labelGetCount = new System.Windows.Forms.Label();
+            this.labelSendCount = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txGet = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.txSend = new System.Windows.Forms.TextBox();
+            this.checkBoxHexSend = new System.Windows.Forms.CheckBox();
+            this.checkBoxNewlineSend = new System.Windows.Forms.CheckBox();
+            this.checkBoxHexView = new System.Windows.Forms.CheckBox();
+            this.checkBoxNewlineGet = new System.Windows.Forms.CheckBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboPortName
+            // 
+            this.comboPortName.BackColor = System.Drawing.Color.White;
+            this.comboPortName.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboPortName.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboPortName.FormattingEnabled = true;
+            this.comboPortName.Location = new System.Drawing.Point(78, 11);
+            this.comboPortName.Name = "comboPortName";
+            this.comboPortName.Size = new System.Drawing.Size(121, 20);
+            this.comboPortName.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Port name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(205, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Baudrate";
+            // 
+            // comboBaudrate
+            // 
+            this.comboBaudrate.BackColor = System.Drawing.Color.White;
+            this.comboBaudrate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBaudrate.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.comboBaudrate.FormattingEnabled = true;
+            this.comboBaudrate.Items.AddRange(new object[] {
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBaudrate.Location = new System.Drawing.Point(264, 11);
+            this.comboBaudrate.Name = "comboBaudrate";
+            this.comboBaudrate.Size = new System.Drawing.Size(121, 20);
+            this.comboBaudrate.TabIndex = 5;
+            // 
+            // buttonOpenClose
+            // 
+            this.buttonOpenClose.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonOpenClose.Font = new System.Drawing.Font("宋体", 9F);
+            this.buttonOpenClose.ForeColor = System.Drawing.Color.Black;
+            this.buttonOpenClose.Location = new System.Drawing.Point(405, 10);
+            this.buttonOpenClose.Name = "buttonOpenClose";
+            this.buttonOpenClose.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpenClose.TabIndex = 0;
+            this.buttonOpenClose.Text = "Open";
+            this.buttonOpenClose.UseVisualStyleBackColor = true;
+            this.buttonOpenClose.Click += new System.EventHandler(this.buttonOpenClose_Click);
+            // 
+            // labelGetCount
+            // 
+            this.labelGetCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelGetCount.AutoSize = true;
+            this.labelGetCount.Font = new System.Drawing.Font("宋体", 9F);
+            this.labelGetCount.Location = new System.Drawing.Point(511, -2);
+            this.labelGetCount.Name = "labelGetCount";
+            this.labelGetCount.Size = new System.Drawing.Size(35, 12);
+            this.labelGetCount.TabIndex = 9;
+            this.labelGetCount.Text = "Get:0";
+            // 
+            // labelSendCount
+            // 
+            this.labelSendCount.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.labelSendCount.AutoSize = true;
+            this.labelSendCount.Font = new System.Drawing.Font("宋体", 9F);
+            this.labelSendCount.Location = new System.Drawing.Point(481, -2);
+            this.labelSendCount.Name = "labelSendCount";
+            this.labelSendCount.Size = new System.Drawing.Size(41, 12);
+            this.labelSendCount.TabIndex = 13;
+            this.labelSendCount.Text = "Send:0";
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonReset.Font = new System.Drawing.Font("宋体", 9F);
+            this.buttonReset.ForeColor = System.Drawing.Color.Black;
+            this.buttonReset.Location = new System.Drawing.Point(486, 10);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 1;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txGet);
+            this.groupBox1.Controls.Add(this.labelGetCount);
+            this.groupBox1.Location = new System.Drawing.Point(15, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(546, 357);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data received ";
+            // 
+            // txGet
+            // 
+            this.txGet.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txGet.Dock = System.Windows.Forms.DockStyle.Left;
+            this.txGet.Location = new System.Drawing.Point(3, 17);
+            this.txGet.Multiline = true;
+            this.txGet.Name = "txGet";
+            this.txGet.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txGet.Size = new System.Drawing.Size(533, 337);
+            this.txGet.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.buttonSend);
+            this.groupBox2.Controls.Add(this.txSend);
+            this.groupBox2.Controls.Add(this.labelSendCount);
+            this.groupBox2.Location = new System.Drawing.Point(15, 402);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(554, 54);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Data send                   ";
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonSend.Font = new System.Drawing.Font("宋体", 9F);
+            this.buttonSend.ForeColor = System.Drawing.Color.Black;
+            this.buttonSend.Location = new System.Drawing.Point(465, 20);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // txSend
+            // 
+            this.txSend.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txSend.Location = new System.Drawing.Point(6, 22);
+            this.txSend.Name = "txSend";
+            this.txSend.Size = new System.Drawing.Size(453, 21);
+            this.txSend.TabIndex = 1;
+            // 
+            // checkBoxHexSend
+            // 
+            this.checkBoxHexSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBoxHexSend.AutoSize = true;
+            this.checkBoxHexSend.Location = new System.Drawing.Point(84, 400);
+            this.checkBoxHexSend.Name = "checkBoxHexSend";
+            this.checkBoxHexSend.Size = new System.Drawing.Size(42, 16);
+            this.checkBoxHexSend.TabIndex = 11;
+            this.checkBoxHexSend.Text = "Hex";
+            this.checkBoxHexSend.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxNewlineSend
+            // 
+            this.checkBoxNewlineSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBoxNewlineSend.AutoSize = true;
+            this.checkBoxNewlineSend.Location = new System.Drawing.Point(132, 400);
+            this.checkBoxNewlineSend.Name = "checkBoxNewlineSend";
+            this.checkBoxNewlineSend.Size = new System.Drawing.Size(72, 16);
+            this.checkBoxNewlineSend.TabIndex = 12;
+            this.checkBoxNewlineSend.Text = "New line";
+            this.checkBoxNewlineSend.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxHexView
+            // 
+            this.checkBoxHexView.AutoSize = true;
+            this.checkBoxHexView.Location = new System.Drawing.Point(109, 36);
+            this.checkBoxHexView.Name = "checkBoxHexView";
+            this.checkBoxHexView.Size = new System.Drawing.Size(72, 16);
+            this.checkBoxHexView.TabIndex = 7;
+            this.checkBoxHexView.Text = "Hex view";
+            this.checkBoxHexView.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxNewlineGet
+            // 
+            this.checkBoxNewlineGet.AutoSize = true;
+            this.checkBoxNewlineGet.Location = new System.Drawing.Point(181, 36);
+            this.checkBoxNewlineGet.Name = "checkBoxNewlineGet";
+            this.checkBoxNewlineGet.Size = new System.Drawing.Size(96, 16);
+            this.checkBoxNewlineGet.TabIndex = 8;
+            this.checkBoxNewlineGet.Text = "Auto newline";
+            this.checkBoxNewlineGet.UseVisualStyleBackColor = true;
+            this.checkBoxNewlineGet.CheckedChanged += new System.EventHandler(this.checkBoxNewlineGet_CheckedChanged);
+            // 
+            // chart1
+            // 
+            this.chart1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.AxisX.LabelStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            chartArea1.AxisX.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            chartArea1.AxisX.LineWidth = 2;
+            chartArea1.AxisX.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            chartArea1.AxisX.MajorGrid.LineWidth = 0;
+            chartArea1.AxisY.LabelStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            chartArea1.AxisY.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            chartArea1.AxisY.LineWidth = 2;
+            chartArea1.AxisY.MajorGrid.LineColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            chartArea1.AxisY.MajorGrid.LineWidth = 0;
+            chartArea1.BackColor = System.Drawing.Color.Transparent;
+            chartArea1.CursorX.IntervalOffsetType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.CursorX.IntervalType = System.Windows.Forms.DataVisualization.Charting.DateTimeIntervalType.Number;
+            chartArea1.Name = "ChartArea1";
+            chartArea1.ShadowColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Alignment = System.Drawing.StringAlignment.Far;
+            legend1.BackColor = System.Drawing.Color.Transparent;
+            legend1.Docking = System.Windows.Forms.DataVisualization.Charting.Docking.Top;
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(567, 11);
+            this.chart1.Name = "chart1";
+            this.chart1.Palette = System.Windows.Forms.DataVisualization.Charting.ChartColorPalette.Bright;
+            series1.BackImageAlignment = System.Windows.Forms.DataVisualization.Charting.ChartImageAlignmentStyle.TopRight;
+            series1.BorderWidth = 2;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(145)))), ((int)(((byte)(155)))));
+            series1.Legend = "Legend1";
+            series1.Name = "直径";
+            series1.Points.Add(dataPoint1);
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(588, 488);
+            this.chart1.TabIndex = 13;
+            // 
+            // SerialportSampleForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(963, 468);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.checkBoxNewlineGet);
+            this.Controls.Add(this.checkBoxNewlineSend);
+            this.Controls.Add(this.checkBoxHexSend);
+            this.Controls.Add(this.checkBoxHexView);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonOpenClose);
+            this.Controls.Add(this.comboBaudrate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboPortName);
+            this.Name = "SerialportSampleForm";
+            this.Text = "Serial tool Sample";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboPortName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBaudrate;
+        private System.Windows.Forms.Button buttonOpenClose;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label labelSendCount;
+        private System.Windows.Forms.Label labelGetCount;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txGet;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox checkBoxNewlineSend;
+        private System.Windows.Forms.CheckBox checkBoxHexSend;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox txSend;
+        private System.Windows.Forms.CheckBox checkBoxHexView;
+        private System.Windows.Forms.CheckBox checkBoxNewlineGet;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
